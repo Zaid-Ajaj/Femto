e47cc1dfde5a652a88b5dc12158b746acecbd086
Maxime Mangel <maxime.shadaen@protonmail.com>
2019-06-21 17:34:07 +0200

    Remove InstallHint property by finding on the fly the version to install
    Fix #3

--- Npm.fs
+++ Npm.fs
@@ -1,14 +1,17 @@
 module Femto.Npm
 
+open Serilog
 open System.IO
 open System.Xml
 open SemVer
+
+let logger = LoggerConfiguration().WriteTo.Console().CreateLogger()
 
 type NpmDependency = {
     Name: string
     Constraint: Range option
     RawVersion : string
-    InstallHint: string
+    LowestMatching : bool option
 }
 
 type InstalledNpmPackage = {
@@ -30,11 +33,26 @@ let elementsByTag (el: XmlElement) tag =
     let elements = el.GetElementsByTagName tag
     [ for i in 0 .. elements.Count - 1 -> elements.Item i ]
 
-let attr (name: string) (node: XmlNode)  =
+let tryAttr (name: string) (node: XmlNode) =
     [ for i in 0 .. node.Attributes.Count - 1 -> node.Attributes.Item(i) :?> XmlAttribute ]
     |> List.tryFind (fun attr -> attr.Name = name)
     |> Option.map (fun attr -> attr.Value)
+
+let attr (name: string) (node: XmlNode)  =
+    tryAttr name node
     |> Option.defaultValue ""
+
+let tryBoolAttr (name : string) (node : XmlNode) =
+    tryAttr name node
+    |> Option.map (fun value ->
+        match System.Boolean.TryParse(value) with
+        | true, res ->
+            res
+
+        | false, _ ->
+            logger.Warning("Invalid value for '{AttributeName}' attribute. Accepted values are 'true' or 'false'", name)
+            true
+    )
 
 let parseDependencies (project: string) =
     try
@@ -43,15 +61,26 @@ let parseDependencies (project: string) =
         let npmDependencies = elementsByTag doc.DocumentElement "NpmDependencies"
         match npmDependencies with
         | [ rootNode ] ->
-            "NpmPackage"
-            |> elementsByTag (rootNode :?> XmlElement)
-            |> List.map (fun node -> {
-                   Name = attr "Name" node;
-                   RawVersion = attr "Version" node
-                   Constraint = parseConstraint (attr "Version" node)
-                   InstallHint = attr "InstallHint" node
-                })
+            let lowestMatching =
+                "Strategy"
+                |> elementsByTag (rootNode :?> XmlElement)
+                |> List.head
+                |> tryBoolAttr "LowestMatching"
+                |> Option.defaultValue true
+
+            let npmPackages =
+                "NpmPackage"
+                |> elementsByTag (rootNode :?> XmlElement)
+                |> List.map (fun node -> {
+                        Name = attr "Name" node;
+                        RawVersion = attr "Version" node
+                        Constraint = parseConstraint (attr "Version" node)
+                        LowestMatching = tryBoolAttr "LowestMatching" node
+                    })
+
+            lowestMatching, npmPackages
         | _ ->
-            []
+            true, []
     with
-    | ex -> []
+        | ex ->
+            true, []
