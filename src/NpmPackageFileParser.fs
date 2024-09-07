e0bb814659dd575d8f1ac6a5765abe93218c1cc5
Zaid Ajaj <zaid.naom@gmail.com>
2024-09-07 17:34:09 +0200

    Femto v0.20 Support package.yaml and package.json5 (thanks to #105)

--- src/NpmPackageFileParser.fs
+++ src/NpmPackageFileParser.fs
@@ -39,7 +39,7 @@ module Yaml =
         | Legivel.Serialization.Success s :: _ -> Ok s.Data
         | Legivel.Serialization.Error e :: _ ->
             e.Error
-            |> List.map _.Message
+            |> List.map (fun x -> x.Message)
             |> String.concat "\n"
             |> Error
         | [] -> Error "No YAML document found"
